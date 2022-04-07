e81d0835dfaf142008937f1783d8095472c7270e
ayodapo1 <99778319+ayodapo1@users.noreply.github.com>
2022-04-07 14:42:18 -0500

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # TOMCAT.SH
-# Steps for Installing tomcat9 on rhel7&8
+# Steps for Installing tomcat10 on rhel7&8
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 # https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
-cd /opt 
+sudo cd /opt 
 sudo yum install git wget -y
 
 sudo yum install java-1.8.0-openjdk-devel -y
@@ -12,9 +12,9 @@ sudo yum install java-1.8.0-openjdk-devel -y
 
 
 # dowanload and extract tomcat software
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.16/bin/apache-tomcat-10.0.16.tar.gz
-sudo tar -xvf apache-tomcat-10.0.16.tar.gz
-sudo rm apache-tomcat-10.0.16.tar.gz
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.20/bin/apache-tomcat-10.0.20.tar.gz
+sudo tar -xvf apache-tomcat-10.0.20.tar.gz
+sudo rm apache-tomcat-10.0.20.tar.gz
 
 sudo chmod 777 -R /opt/tomcat10
 sudo sh /opt/tomcat10/bin/startup.sh
@@ -32,7 +32,7 @@ echo "end on tomcat installation"
 find / -name server.xml context.xml
 vim /opt/tomcat10/conf/server.xml
 vi /opt/tomcat10/webapps/manager/META-INF/context.xml
-vi /opt/tomcat10/conf/tomcat-user.xml  # to add user
+vi /opt/tomcat10/conf/tomcat-users.xml  # to add user
 
 	<user username="landmark" password="admin123" roles="manager-gui,admin-gui,manager-script"/>
 	
@@ -41,7 +41,7 @@ vi /opt/tomcat10/conf/tomcat-user.xml  # to add user
 
  vi /opt/tomcat10/webapps/manager/META-INF/context.xml
   
-  vi /opt/tomcat10/conf/tomcat-user.xml  # to add user
+  vi /opt/tomcat10/conf/tomcat-users.xml  # to add user
   
 	
 	username YourName password=PassWord   roles=manager-gui
